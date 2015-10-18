cdd090126dfbf5ff20d3928165a9f68f531782aa
macbellingrath <macbellingrath@gmail.com>
2015-10-18 10:45:41 -0400

    List comprehension in Haskell

--- Hi.hs
+++ Hi.hs
@@ -1,7 +1,15 @@
-main = print (quadruple 5)
+main = print()
 
 
 double x = x + x
 
 
 quadruple x = double x + double x
+
+doubleSmallNumber x = if x > 100
+			 		  	then x
+			 		    else x * 2
+listComprehension y = [x * 2 | 
+					   x <- [1..y]]
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
